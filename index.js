be44ba7ac208ce48a4983ed744d38d82c1bf825c
Louell Sala <louell_sala@yahoo.com>
2022-02-25 17:57:06 -0500

    changed to exhlo

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ export default class HelloVR extends React.Component {
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
           <Text style={styles.greeting}>
-           Lets pray for Ukraine
+         Welcome to EXHLO
           </Text>
         </View>
       </View>
